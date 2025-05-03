47014f2a7c0ed3037c35a2c27533dbba8e7d39c8
Deva <writetodevnarayan@gmail.com>
2025-05-03 18:19:32 +0530

    backend file upload

--- backend/src/s3/s3.service.ts
+++ backend/src/s3/s3.service.ts
@@ -1,4 +1,4 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
@@ -10,6 +10,7 @@ export class S3Service {
     constructor(private config:ConfigService){
         this.bucket=config.get<string>('S3_BUCKET');
         this.s3=new S3Client({
+            region: 'us-east-1',
             endpoint:config.get<string>('S3_ENDPOINT'),
             forcePathStyle:true,
             credentials:{
@@ -20,6 +21,15 @@ export class S3Service {
     }
 
     async uploadFile(file:Express.Multer.File, employeeId:string){
-        const key= `${employeeId}`
+        const key= `${employeeId}/${Date.now()}-${file.originalname}`;
+        const command= new PutObjectCommand({
+            Bucket:this.bucket,
+            Key:key,
+            Body:file.buffer,
+            ContentType:file.mimetype
+        });
+        await this.s3.send(command);
+        const url=`${this.s3.config.endpoint}/${this.bucket}/${key}`;
+        return {url,key};
     }
 }
--- backend/src/upload/upload.controller.ts
+++ backend/src/upload/upload.controller.ts
@@ -3,52 +3,19 @@ import { FileInterceptor } from "@nestjs/platform-express";
 import { extname, join } from "path";
 import {diskStorage} from 'multer'
 import * as fs from 'fs'
+import { S3Service } from "src/s3/s3.service";
 
 @Controller('upload')
 export class UploadController{
     private readonly logger=new Logger(UploadController.name);
+    constructor(private readonly s3Service:S3Service){}
     @Post(':id')
-    @UseInterceptors(
-        FileInterceptor('file',{
-            storage:diskStorage({
-                destination:(req,file,cb)=>{
-                    const userId=req.params.id;
-                    const dir=`./uploads/${userId}`;
-                    if (!fs.existsSync(dir)) {
-                        fs.mkdirSync(dir, { recursive: true });
-                        console.log(`Directory created: ${dir}`);
-                      }
-                    cb(null,dir);
-                },
-                filename:(req,file,cb)=>{
-                    const unqiueSuffix = Date.now()+ '-' + Math.round(Math.random()*1e9);
-                    console.log(`Generated filename: ${unqiueSuffix}${extname(file.originalname)}`);
-                    cb(null, `${unqiueSuffix}${extname(file.originalname)}`);
-                },
-            }),
-        }),
-    )
-    uploadFile(@Param('id') userId:string, @UploadedFile() file:Express.Multer.File){
-        this.logger.log(`Uploaded file for user ${userId}: ${file.filename}`);
-        return{
-            filename:file.filename,
-            message: 'File uploaded successfully',
-            path:`http://localhost:3000/uploads/${userId}/${file.filename}`,
-            userId,
-        };
-    }
-
-    @Get(':id')
-    getUploadedFiles(@Param('id') userId:string){
-        const userDir=join(__dirname,'..','..','uploads',userId);
-        if(!fs.existsSync(userDir)){
-            this.logger.warn(`Directory not found for ${userId}`);
-            return [];
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadFile(@Param('id') employeeId:string, @UploadedFile() file:Express.Multer.File){
+        const res=await this.s3Service.uploadFile(file,employeeId);
+        return {
+            message:'Upload successful',
+            url:res.url
         }
-        const files=fs.readdirSync(userDir);
-        return files.map((file)=>({
-            filename:file,
-            path:`http://localhost:3000/uploads/${userId}/${file}`
-        }))
     }
 }
